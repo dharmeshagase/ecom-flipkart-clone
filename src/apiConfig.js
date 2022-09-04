b829aa587765d6725611875e0ad244325e7b5489
Dharmesh Agase <you@example.com>
2022-09-04 18:42:37 +0530

    Updated baseurl to production url

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,4 +1,6 @@
-export const api = `http://localhost:2000/api`;
+// export const api = `http://localhost:2000/api`;
+const baseURL = "https://flipkart-backend-rest-server.herokuapp.com";
+export const api = `${baseURL}/api`;
 export const generatePublicImageUrl = (filename)=>{
-    return `http://localhost:2000/public/${filename}`;
+    return `${baseURL}/public/${filename}`;
 }
